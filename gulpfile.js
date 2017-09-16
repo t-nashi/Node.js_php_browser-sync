7b14bfe283545fbb5d941328c171056a0830c257
TsutomuTakanashi <t_19nashi@yahoo.co.jp>
2017-09-16 21:34:44 +0900

    必要ファイルをコミット

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+// モジュール読み込み
+var gulp = require("gulp");								// gulpを呼び出す
+var browser = require("browser-sync");					// LiveReload環境構築
+var connect = require("gulp-connect-php");              // phpを扱うようにする
+
+// サーバー実行
+gulp.task("server", function() {
+    connect.server({
+        base:'./public',                    // ベースとなるフォルダ
+        port: 8000,
+        bin: 'C:/xampp/php/php.exe',        // xamppのphp.exe （xamppのApacheの起動は不要。必要があればパスを通す。）
+        ini: 'C:/xampp/php/php.ini'         // xamppのphp.ini
+    }, function (){
+        browser({
+            proxy: 'localhost:8000/',       // proxyを設定することによってこの指定ページを表示するようになる
+            files: './public/**/*.*',       // 監視対象のファイル
+            port: 3000                      // port番号
+        });
+    });
+});
+
+// 「gulp」で実行のタスク （ファイルの監視/js/sass、サーバー実行）
+gulp.task("default",['server'], function() {
+    //
+});
